b4885baf5e923418960b39efe32221d7d7b5e6d1
Francisco Moreno Sanchez <franmoreno1506@gmail.com>
2021-08-11 01:10:02 +0200

    Fix: Arreglado problema con las reacciones
    ¡PENDING! Arreglar problema con el autoedit de las reacciones

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const intents = new Discord.Intents(32767)
 const client = new Discord.Client({ intents })
 
-const { Client, MessageEmbed } = require('discord.js')
+const { Client, MessageEmbed, guild } = require('discord.js')
 
 module.exports = {
     name: "help",
@@ -11,14 +11,35 @@ module.exports = {
 execute (client, message, args){
 
     const embedPrincipal = new Discord.MessageEmbed()
-        .setTitle("Bienvenido al sistema de soporte del bot")
-        .setDescription("A continuacion por favor reaccione al mensaje segun el apartado que quiera acceder")
+        .setTitle("Bienvenido al centro de ayuda")
+        .setDescription("A continuacion por favor ejecute el comando segun el apartado que desee")
+        .setFields(
+            { name: 'Reacciones', value: '\n\n***:keyboard: Diccionario de comandos ***\n\n***:scroll:  Normas y reglas del servidor ***\n\n***:clock3: Horarios de stream ***'}
+        )
         .setColor('BLACK')
-        .setAuthor('Racks')
+        .setFooter('Para mas soporte contacte con el canal de <#874226645823791174>')
+        
+    const embedComandos = new Discord.MessageEmbed()
+        .setTitle('Diccionario de comandos')
+        .setDescription('**$ping** *pong*')
+        .setAuthor('Main developer')
 
-   message.channel.send({ embeds: [embedPrincipal] })
+    message.channel.send({ embeds: embedPrincipal}).then( sentMessage => {
+        sentMessage.react('⌨️')
+        sentMessage.react('📜')
+        sentMessage.react('🕒')
 
+        sentMessage.awaitReactions((reaction, user) => {
+            if(message.author.id !== user.id) return;
+            if(reaction.emoji.name === '📜'){
+                sentMessage.edit(embedComandos)
+            }
+        })
+    })
+   
 
 }
 
 }
+
+
